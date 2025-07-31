886d8ac6be71925ef3d6d29052e959ae61764653
tommasovenza <tommasovenza@gmail.com>
2025-07-31 16:22:03 +0200

    created decoration for username

--- resources/views/layout/home.blade.php
+++ resources/views/layout/home.blade.php
@@ -3,9 +3,8 @@
 @section('content')
     <div class="container pt-10 pb-10">
         <div>
-            <h1 class="text-3xl font-semibold mb-4">Hey there 👋, I'm Tommaso Venza</h1>
-            <p>A Full Stack Web Developer based in the Netherlands
-            With a passion for Laravel, Tailwind CSS, Javascript. Previously worked extensively with Wordpress, still valuing its approach, now focused on Laravel for new projects.</p>
+            <h1 class="text-3xl font-semibold mb-4">Hey there 👋, I'm <span class="name">Tommaso Venza</span></h1>
+            <p>A Full Stack Web Developer based in the Italy with a passion for Laravel, CSS, Javascript.</p>
         </div>
     </div>
 @endsection
