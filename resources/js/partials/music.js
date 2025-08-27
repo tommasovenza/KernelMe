68d4a481981cfbacad8ae9e8761c15fa814e6687
tommasovenza <tommasovenza@gmail.com>
2025-08-27 18:08:56 +0200

    created modal with clicked images - basic style

--- resources/js/partials/music.js
+++ resources/js/partials/music.js
@@ -1,11 +1,38 @@
 const url = window.location.href
+const musicGrid = document.querySelector('.music-grid-container')
 const musicImages = document.querySelectorAll('.music-card > img')
-console.log(musicImages)
+// console.log(musicImages)
+const modal = document.querySelector('#myModal')
+
+function closeModal(e) {
+    if (e.target.classList.contains('close')) {
+        e.target.parentElement.parentElement.style = ''
+    }
+}
 
 function showMusicCard(e) {
-    console.log(e.target)
+    if (e.target.classList.contains('instrument')) {
+        console.log(e.target)
+        // show modal
+        modal.style.display = 'block'
+        // const musicCard = document.createElement('div')
+        // get src and alt attributes
+        const src = e.target.src
+        const alt = e.target.alt
+        console.log(src)
+        modal.innerHTML = `
+        <div class="modal-content">
+        <span class="close">&times;</span>
+            <img src="${src}" alt="${alt}">
+            <p>${alt}</p>
+        </div>
+        `
+    } else {
+        return
+    }
 }
 
 if (url.includes('music')) {
-    musicImages.forEach(card => card.addEventListener('click', showMusicCard))
+    musicGrid.addEventListener('click', showMusicCard)
+    modal.addEventListener('click', closeModal)
 }
--- resources/views/layout/music.blade.php
+++ resources/views/layout/music.blade.php
@@ -6,32 +6,44 @@
 
 @section('content')
 <section class="music pb-10">
-        <div class="container">
-            <div class="heading-projects p-4">
-                <h2 class="text-center text-2xl text-blue-500 font-semibold">Music</h2>
+    <!-- The Modal -->
+    <div id="myModal" class="modal">
+        <!-- Modal content -->
+        <div class="modal-content">
+            <span class="close">&times;</span>
+            <p>Some text in the Modal..</p>
+        </div>
+    </div>
+    <div class="container">
+        <div class="heading-projects p-4">
+            <h2 class="text-center text-2xl text-blue-500 font-semibold">Music</h2>
+        </div>
+
+        <div class="music-grid-container">
+            <div class="music-card">
+                <img src="{{ asset('/images/music/les-paul.jpg') }}" alt="Gibson Les Paul" class="instrument">
             </div>
 
-            <div class="music-grid-container">
-                <div class="music-card">
-                    <img src="{{ asset('/images/projects/workopia.jpg') }}" alt="test">
-                </div>
+            <div class="music-card">
+                <img src="{{ asset('/images/music/stratocaster.jpg') }}" alt="stratocaster" class="instrument">
+            </div>
 
-                <div class="music-card">
-                    <img src="{{ asset('/images/projects/hangman.jpg') }}" alt="test">
-                </div>
+            <div class="music-card">
+                <img src="{{ asset('/images/music/telecaster.jpg') }}" alt="telecaster" class="instrument">
+            </div>
+            <div class="music-card">
+                <img src="{{ asset('/images/music/telecaster.jpg') }}" alt="telecaster" class="instrument">
+            </div>
 
-                <div class="music-card">
-                    <img src="{{ asset('/images/projects/qr-maker.jpg') }}" alt="test">
-                </div>
-
-                <div class="music-card">
-                    <img src="{{ asset('/images/projects/workopia.jpg') }}" alt="test">
-                </div>
-
-                <div class="music-card">
-                    <img src="{{ asset('/images/projects/workopia.jpg') }}" alt="test">
-                </div>
+            <div class="music-card">
+                <img src="{{ asset('/images/music/les-paul.jpg') }}" alt="Gibson Les Paul" class="instrument">
+            </div>
+            
+            
+            <div class="music-card">
+                <img src="{{ asset('/images/music/stratocaster.jpg') }}" alt="stratocaster" class="instrument">
             </div>
         </div>
-    </section>
+    </div>
+</section>
 @endsection
