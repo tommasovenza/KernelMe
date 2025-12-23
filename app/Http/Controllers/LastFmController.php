0a431ad06399d246e62a5143ce0b9e25be02800f
tommasovenza <tommasovenza@gmail.com>
2025-12-23 16:36:06 +0100

    set some variables as private and used type hint

--- app/Http/Controllers/LastFmController.php
+++ app/Http/Controllers/LastFmController.php
@@ -6,44 +6,22 @@ use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Http;
 
-use function Pest\Laravel\json;
-
 class LastFmController extends Controller
 {
-    // Get Top Tracks call Last FM API
-    public function getTopTracks(): JsonResponse
-    {
-        $user = "tommibommi";
-        $baseEndpoint = "https://ws.audioscrobbler.com/2.0/";
-        $lastFmKey = config('services.last_fm_key');
+    // private
+    private string $user = "tommibommi";
+    private string $baseEndpoint = "https://ws.audioscrobbler.com/2.0/";
 
-        // Call API through, PHP HTTP client => Guzzle
-        $response = Http::get($baseEndpoint, [
-            'method' => 'user.gettoptracks',
-            'user' => $user,
-            'limit' => 10,
-            'api_key' => $lastFmKey,
-            'format' => 'json',
-            'period' => '1month'
-        ])->throw();
-
-        // get JSON
-        $json = $response->json();
-
-        // return json to
-        return response()->json($json, 200);
-    }
-
+    // Recent Track Method
     public function recentTracks(): JsonResponse
     {
-        $user = "tommibommi";
-        $baseEndpoint = "https://ws.audioscrobbler.com/2.0/";
+        // get Last Fm Key from .env file through config services
         $lastFmKey = config('services.last_fm_key');
 
         // Call API through, PHP HTTP client => Guzzle
-        $response = Http::get($baseEndpoint, [
+        $response = Http::get($this->baseEndpoint, [
             'method' => 'user.getrecenttracks',
-            'user' => $user,
+            'user' => $this->user,
             'limit' => 100,
             'api_key' => $lastFmKey,
             'format' => 'json',
