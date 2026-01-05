9bd35a4358e2d0f04b97c19074bf372d9f3a4677
tommasovenza <tommasovenza@gmail.com>
2026-01-05 03:42:36 +0100

    printed post show content

--- resources/views/layout/blog/show.blade.php
+++ resources/views/layout/blog/show.blade.php
@@ -6,7 +6,6 @@
 
 @section('content')
 <div class="container pb-10 blog-show">
-
     {{-- Heading --}}
     <div class="post-heading">
         {{-- Heading Content --}}
@@ -42,18 +41,12 @@
         </div>
     </div>
 
+    @php
+        $html = stripcslashes($post->content);
+    @endphp
 
-    {{-- <h1 class="text-3xl">Blog Page</h1> --}}
-    {{-- <ul class="mt-4 mb-4">
-        <li><strong>Post ID:</strong> {{ $post->id }}</li>
-        <li><strong>Post Title:</strong> {!! $post->title !!}</li>
-        <li><strong>Post Subtitle:</strong> {{ $post->subtitle }}</li>
-        <li><strong>Post Slug:</strong> {{ $post->slug }}</li>
-        <li><strong>Post Content:</strong> {{ $post->content }}</li>
-        <li><strong>Post Excerpt:</strong> {{ $post->excerpt }}</li>
-        <li><strong>Post Date:</strong> {{ $post->date }}</li>
-        <li><strong>Post Tags:</strong> {{ $post->tags }}</li>
-        <li><strong>Post Read Time:</strong> {{ $post->read_time }}</li>
-    </ul> --}}
+    <div class="blog-show mt-12">
+        <div class="post-content text-lg">{!! $html !!}</div>
+    </div>
 </div>
 @endsection
