5bcf9a2622ea07034d7de926044a0cfb43fb5e31
tommasovenza <tommasovenza@gmail.com>
2026-01-08 15:51:39 +0100

    protected sensible routes with middleware auth and throttle

--- routes/web.php
+++ routes/web.php
@@ -21,25 +21,29 @@ Route::get('/privacy-policy', function () {
     return view('layout.privacy');
 })->name('privacy');
 
-// Post Controller
+// Post Controller Routes
 Route::get('/blog', [PostController::class, 'getPostsBlog'])->name('blog');
-Route::get('/blog/create', [PostController::class, 'postCreate'])->name('post.create')->middleware('auth');
-Route::post('/post/store', [PostController::class, 'postStore'])->name('post.store');
+// Admin Routes for adding Posts
+Route::middleware(['auth', 'throttle:20,1'])->group(function () {
+    Route::get('/blog/create', [PostController::class, 'postCreate'])->name('post.create');
+    Route::post('/post/store', [PostController::class, 'postStore'])->name('post.store');
+});
+// Show Post
 Route::get('/post/{post}', [PostController::class, 'postShow'])->name('post.show');
 
 // Contact Form Controller
 Route::get('/contacts', [ContactController::class, 'showContactPage'])->name('contacts');
-Route::post('/contact-form', [ContactController::class, 'submitContactForm'])->name('contact-form');
+Route::post('/contact-form', [ContactController::class, 'submitContactForm'])
+    ->name('contact-form')
+    ->middleware('throttle:10,1');
 
 // User form
 Route::get('/login', [UserController::class, 'showLogin'])->name('show-login');
-Route::post('/auth', [UserController::class, 'authUser'])->name('auth-user');
+Route::post('/auth', [UserController::class, 'authUser'])->name('auth-user')->middleware('throttle:10,1');
 Route::post('/logout', [UserController::class, 'logout'])->name('logout')->middleware('auth');
 
 // Last Fm API
 Route::get('/my-listens', [LastFmController::class, 'showLastFmView'])->name('listens'); // show view
 Route::get('/last-fm/recent-tracks', [LastFmController::class, 'recentTracks']); // recent tracks
-Route::get('/last-7-days', [LastFmController::class, 'last7Days']); // last 7 days
-Route::get('/last-30-days', [LastFmController::class, 'last30Days']); // last 30 days
 Route::get('/top-artist', [LastFmController::class, 'topArtist']); // top artist
 Route::get('/top-album', [LastFmController::class, 'topAlbum']); // top album
