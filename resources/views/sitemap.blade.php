108ab3de1398a265e3b7a26fb66c90db890c1ef1
tommasovenza <tommasovenza@gmail.com>
2026-03-05 10:30:31 +0100

    fixed sitemap

--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -1,6 +1,4 @@
-@php
-echo '<?xml version="1.0" encoding="UTF-8"@endphp'; ?>';
-?>
+@php echo '<?xml version="1.0" encoding="UTF-8"@endphp'; ?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     {{-- Pagine statiche --}}
     <url>
@@ -8,37 +6,31 @@ echo '<?xml version="1.0" encoding="UTF-8"@endphp'; ?>';
         <changefreq>weekly</changefreq>
         <priority>1.0</priority>
     </url>
-
     <url>
         <loc>{{ route('blog') }}</loc>
         <changefreq>weekly</changefreq>
         <priority>0.9</priority>
     </url>
-
     <url>
         <loc>{{ route('music') }}</loc>
         <changefreq>monthly</changefreq>
         <priority>0.5</priority>
     </url>
-
     <url>
         <loc>{{ route('listens') }}</loc>
         <changefreq>daily</changefreq>
         <priority>0.6</priority>
     </url>
-
     <url>
         <loc>{{ route('contacts') }}</loc>
         <changefreq>yearly</changefreq>
         <priority>0.3</priority>
     </url>
-
     <url>
         <loc>{{ route('privacy') }}</loc>
         <changefreq>yearly</changefreq>
         <priority>0.1</priority>
     </url>
-
     {{-- Post del blog --}}
     @foreach ($posts as $post)
         <url>
